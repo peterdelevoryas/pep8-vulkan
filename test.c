481b7e00f5b7b52012c54ab50426be5d0af7964c
peterd <me@peterd.net>
2020-11-28 20:20:39 -0800

    Revert snake-case, go back to pep8
    I'm crazy, I keep going back and forth between all-snake_case and pep8.
    
    This reverts commit efaf6780d924e0233bf9a6257f0f4f7046bdebb4.

--- test.c
+++ test.c
@@ -12,21 +12,21 @@ int main() {
         "VK_EXT_debug_report",
         "VK_EXT_debug_utils",
     };
-    struct vk_instance_create_info info = {
+    VkInstanceCreateInfo info = {
         .s_type                     = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         .enabled_extension_count    = 4,
         .pp_enabled_extension_names = extensions,
     };
-    struct vk_instance* instance;
+    VkInstance instance;
     vk_create_instance(&info, NULL, &instance);
     assert(instance);
 
     uint32_t           physical_device_count = 1;
-    struct vk_physical_device* physical_device;
+    VkPhysicalDevice physical_device;
     vk_enumerate_physical_devices(instance, &physical_device_count, &physical_device);
     assert(physical_device);
 
-    struct vk_physical_device_properties properties;
+    VkPhysicalDeviceProperties properties;
     vk_get_physical_device_properties(physical_device, &properties);
     printf("Using physical device '%s'\n", properties.device_name);
 
--- vulkan_xcb.h
+++ vulkan_xcb.h
@@ -19,31 +19,32 @@ extern "C" {
 #define vk_khr_xcb_surface 1
 #define VK_KHR_XCB_SURFACE_SPEC_VERSION 6
 #define VK_KHR_XCB_SURFACE_EXTENSION_NAME "VK_KHR_xcb_surface"
-typedef vk_flags vk_xcb_surface_create_flags_khr;
-typedef struct vk_xcb_surface_create_info_khr {
-    vk_structure_type               s_type;
-    const void*                     p_next;
-    vk_xcb_surface_create_flags_khr flags;
-    xcb_connection_t*               connection;
-    xcb_window_t                    window;
-} vk_xcb_surface_create_info_khr;
+typedef VkFlags VkXcbSurfaceCreateFlagsKHR;
+typedef struct VkXcbSurfaceCreateInfoKHR {
+    VkStructureType            s_type;
+    const void*                p_next;
+    VkXcbSurfaceCreateFlagsKHR flags;
+    xcb_connection_t*          connection;
+    xcb_window_t               window;
+} VkXcbSurfaceCreateInfoKHR;
 
-typedef vk_result(VKAPI_PTR* pfn_vk_create_xcb_surface_khr)(vk_instance                           instance,
-                                                            const vk_xcb_surface_create_info_khr* p_create_info,
-                                                            const vk_allocation_callbacks*        p_allocator,
-                                                            vk_surface_khr*                       p_surface);
-typedef vk_bool32(VKAPI_PTR* pfn_vk_get_physical_device_xcb_presentation_support_khr)(
-    vk_physical_device physical_device, uint32_t queue_family_index, xcb_connection_t* connection,
-    xcb_visualid_t visual_id);
+typedef VkResult(VKAPI_PTR* pfn_vk_create_xcb_surface_khr)(VkInstance                       instance,
+                                                           const VkXcbSurfaceCreateInfoKHR* p_create_info,
+                                                           const VkAllocationCallbacks*     p_allocator,
+                                                           VkSurfaceKHR*                    p_surface);
+typedef VkBool32(VKAPI_PTR* pfn_vk_get_physical_device_xcb_presentation_support_khr)(VkPhysicalDevice physical_device,
+                                                                                     uint32_t queue_family_index,
+                                                                                     xcb_connection_t* connection,
+                                                                                     xcb_visualid_t    visual_id);
 
 #ifndef VK_NO_PROTOTYPES
-VKAPI_ATTR vk_result VKAPI_CALL vk_create_xcb_surface_khr(vk_instance                           instance,
-                                                          const vk_xcb_surface_create_info_khr* p_create_info,
-                                                          const vk_allocation_callbacks*        p_allocator,
-                                                          vk_surface_khr* p_surface) __asm("vkCreateXcbSurfaceKHR");
+VKAPI_ATTR VkResult VKAPI_CALL vk_create_xcb_surface_khr(VkInstance                       instance,
+                                                         const VkXcbSurfaceCreateInfoKHR* p_create_info,
+                                                         const VkAllocationCallbacks*     p_allocator,
+                                                         VkSurfaceKHR* p_surface) __asm("vkCreateXcbSurfaceKHR");
 
-VKAPI_ATTR vk_bool32 VKAPI_CALL vk_get_physical_device_xcb_presentation_support_khr(
-    vk_physical_device physical_device, uint32_t queue_family_index, xcb_connection_t* connection,
+VKAPI_ATTR VkBool32 VKAPI_CALL vk_get_physical_device_xcb_presentation_support_khr(
+    VkPhysicalDevice physical_device, uint32_t queue_family_index, xcb_connection_t* connection,
     xcb_visualid_t visual_id) __asm("vkGetPhysicalDeviceXcbPresentationSupportKHR");
 #endif
 
